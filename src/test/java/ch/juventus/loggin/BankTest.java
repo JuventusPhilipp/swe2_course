262395f43b0e91636b880a0d309741c0acde7149
anon.battle <get@lost.com>
2021-03-04 20:13:13 +0100

    new pom with exclusions for junit

--- src/test/java/ch/juventus/loggin/BankTest.java
+++ src/test/java/ch/juventus/loggin/BankTest.java
@@ -1,36 +1,36 @@
 package ch.juventus.loggin;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BankTest {
 
     private Bank bank;
 
     @BeforeEach
-    void setup() {
+    public void setup() {
         bank = new Bank(500);
     }
 
     @Test
-    void shouldWithdrawTen() throws Exception {
-        assertEquals(490, this.bank.withdraw(10));
+    public void shouldWithdrawTen() throws Exception {
+        assertEquals(490, bank.withdraw(10));
     }
 
     @Test
-    void shouldThrowException() {
+    public void shouldThrowException() {
         assertThrows(Exception.class, () -> this.bank.withdraw(1000));
     }
 
     // deposit
     @Test
-    void shouldDepositTen() {
+    public void shouldDepositTen() {
         assertEquals(510, this.bank.deposit(10));
     }
 
     @Test
-    void shouldReturnBalance() {
+    public void shouldReturnBalance() {
         assertEquals(500, this.bank.retrieveBalance());
     }
 }
